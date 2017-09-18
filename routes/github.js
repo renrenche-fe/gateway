58c17a4f7255f430b30490f810f4d1420db06d03
sunhengzhe <825980764@qq.com>
2017-09-18 09:10:51 +0800

    add comments

--- routes/github.js
+++ routes/github.js
@@ -3,10 +3,24 @@ var request = require('axios');
 var logger = require('winston');
 var router = express.Router();
 
+// github event type
+const GITHUB_EVENT_TYPE = {
+	ISSUES: 'issues',
+	ISSUE_COMMENT: 'issue_comment',
+};
+
+// event action
+const ACTION = {
+	CREATED: 'created',
+	OPENED: 'opened',
+};
+
 /* POST github notify */
-router.post('/notify', function(req, res, next) {
+router.post('/notify', function (req, res, next) {
+	const eventType = req.headers['x-github-event'];
+	// dingtalk webhook
 	const dingtalkUrl = req.query.dingtalk;
-	const type = req.headers['x-github-event'];
+
 	const {
 		action,
 		comment,
@@ -14,15 +28,29 @@ router.post('/notify', function(req, res, next) {
 		sender,
 		repository,
 	} = req.body;
+
 	const user = sender.login;
-	
+	// issue
+	const issueTitle = issue.title;
+	const issueUrl = issue.html_url;
+	// repo
+	const repoName = repository.name;
+
 	let params = null;
 
-	if (type === 'issue_comment' && action === 'created') {
-		// create a issue comment
-		const issueTitle = issue.title;
-		const issueUrl = issue.html_url;
+	if (eventType === GITHUB_EVENT_TYPE.ISSUES && action === ACTION.OPENED) {
+		// opened a issue
+		params = {
+			msgtype: 'markdown',
+			markdown: {
+				title: `${user} 在 ${repoName} 下添加了 issue`,
+				text: `${user} 在 ${repoName} 下添加了 issue：[${issueTitle}](${issueUrl})`,
+			}
+		}
+	}
 
+	if (eventType === GITHUB_EVENT_TYPE.ISSUE_COMMENT && action === ACTION.CREATED) {
+		// created a issue comment
 		params = {
 			msgtype: 'markdown',
 			markdown: {
@@ -32,28 +60,15 @@ router.post('/notify', function(req, res, next) {
 		}
 	}
 
-	if (type === 'issues' && action === 'opened') {
-		const issueTitle = issue.title;
-		const issueUrl = issue.html_url;
-		const repoName = repository.name;
-		
-		params = {
-                        msgtype: 'markdown',
-                        markdown: {
-                                title: `${user} 在 ${repoName} 下添加了 issue`,
-                                text: `${user} 在 ${repoName} 下添加了 issue：[${issueTitle}](${issueUrl})`,
-                        }
-                }		
-        }
-
 	if (!params) {
-		return res.send('no content');
+		return res.status(400).send('not supported event type');
 	}
-	
+
+	// dingtalk notify
 	request.post(dingtalkUrl, params).then(() => {
 		res.send(params);
 	}).catch(err => {
-		res.send(err);
+		res.status(500).send(err);
 	});
 });
 
